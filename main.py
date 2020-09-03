60a6e017d45754a2de3d5b56f937bb48e1cb174b
Rikuta Hiratsuka <rimuru930@gmail.com>
2020-09-04 00:34:39 +0900

    リファクタリング: 余分なコードを削除した｡

--- main.py
+++ main.py
@@ -14,11 +14,8 @@ hit_per_page = 10
 
 
 def input_user_address():
-    disable_list = ['<', '>']
     add_1 = input('都道府県を入力>>>')
     address_1 = add_1.replace('<', '')
-
-    # abcdefghijklmnopqrstuvwxyz!"#$%&()-=^~¥|@`[]{}:*;+<>,./?_
     add_2 = input('市区町村を入力>>>')
     address_2 = add_2.replace('<', ' ')
     return address_1 + address_2
